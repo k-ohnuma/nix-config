cab83db1f90d4dd80a0a18785df4bfd10344b587
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-15 07:16:55 +0900

    feat: darwin-modules最低限定義

--- outputs/aarch64-darwin/src/user.nix
+++ outputs/aarch64-darwin/src/user.nix
@@ -1,16 +1,38 @@
-{ libx, ... }@args:
+{
+  inputs,
+  lib,
+  libx,
+  system,
+  ...
+}@args:
 let
   hostName = "user";
   userName = "user";
 
   modules = {
-    darwin-modules = [ ];
+    darwin-modules = [
+      {
+        users.users.${userName}.home = "/Users/${userName}";
+        system.primaryUser = userName;
+        system.stateVersion = 6;
+        nix.enable = false;
+      }
+    ];
     home-modules = map libx.relativeToRoot [
       "home/hosts/darwin/${hostName}.nix"
     ];
   };
 
-  systemArgs = modules // args // { inherit hostName userName; };
+  systemArgs = modules // args // {
+    inherit
+      hostName
+      inputs
+      lib
+      libx
+      system
+      userName
+      ;
+  };
 in
 {
   darwinConfigurations.${hostName} = libx.macosSystem systemArgs;
