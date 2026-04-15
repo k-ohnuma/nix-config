d4d855ea56e87fc220092c5bb07015b127696c10
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-15 09:13:35 +0900

    feat: set default editor

--- home/base/home.nix
+++ home/base/home.nix
@@ -8,5 +8,9 @@
     # - 新しいHome Manager releaseに後方互換のない変更が入った時の破壊を避ける
     # - Home Manager を更新しても、この値を変える必要はない
     stateVersion = "25.11";
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
   };
 }
