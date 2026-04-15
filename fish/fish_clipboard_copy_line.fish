e45ec25b583e703d8ca15f4cfcb06939f14d376d
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-16 08:46:35 +0900

    fix(fish): pbcopyがない場合のfallback

--- fish/fish_clipboard_copy_line.fish
+++ fish/fish_clipboard_copy_line.fish
@@ -1,3 +1,16 @@
 function fish_clipboard_copy_line
-  commandline | pbcopy
+  set -l text (commandline)
+
+  if type -q pbcopy
+    printf '%s' "$text" | pbcopy
+  else if type -q wl-copy
+    printf '%s' "$text" | wl-copy
+  else if type -q xclip
+    printf '%s' "$text" | xclip -selection clipboard
+  else if type -q xsel
+    printf '%s' "$text" | xsel --clipboard --input
+  else
+    echo "clipboard command not found: pbcopy, wl-copy, xclip, or xsel" >&2
+    return 1
+  end
 end
--- fish/fish_clipboard_copy_word.fish
+++ fish/fish_clipboard_copy_word.fish
@@ -7,6 +7,18 @@ function fish_clipboard_copy_word
 
   set -l left_word (string replace -r '^.*[[:space:]]' '' -- $left)
   set -l right_word (string replace -r '[[:space:]].*$' '' -- $right)
+  set -l text "$left_word$right_word"
 
-  printf '%s%s' $left_word $right_word | pbcopy
+  if type -q pbcopy
+    printf '%s' "$text" | pbcopy
+  else if type -q wl-copy
+    printf '%s' "$text" | wl-copy
+  else if type -q xclip
+    printf '%s' "$text" | xclip -selection clipboard
+  else if type -q xsel
+    printf '%s' "$text" | xsel --clipboard --input
+  else
+    echo "clipboard command not found: pbcopy, wl-copy, xclip, or xsel" >&2
+    return 1
+  end
 end
