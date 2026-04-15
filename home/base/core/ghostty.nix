7b4204e43659eaeb64be217f7e7cb54e97457d11
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-15 22:30:46 +0900

    feat: add ghostty

---
+++ home/base/core/ghostty.nix
@@ -0,0 +1,34 @@
+{
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      font-family = "Bizin Gothic NF";
+      font-size = 15;
+      theme = "Ayu Mirage";
+
+      cursor-style = "block";
+      cursor-color = "f0fcff";
+      cursor-style-blink = false;
+      shell-integration-features = "no-cursor";
+
+      window-show-tab-bar = "never";
+      background-opacity = 0.9;
+      quick-terminal-position = "center";
+      quick-terminal-screen = "main";
+      quick-terminal-animation-duration = 0.3;
+
+      keybind = [
+        "cmd+u=next_tab"
+        "cmd+y=previous_tab"
+        "ctrl+b>-=new_split:down"
+        "ctrl+b>alt+\\=new_split:right"
+        "alt+l=goto_split:right"
+        "alt+k=goto_split:up"
+        "alt+j=goto_split:down"
+        "alt+h=goto_split:left"
+        "global:ctrl+shift+tab=toggle_quick_terminal"
+      ];
+    };
+  };
+}
---
+++ home/darwin/ghostty.nix
@@ -0,0 +1,9 @@
+{
+  programs.ghostty = {
+    settings = {
+      macos-titlebar-style = "tabs";
+      macos-window-buttons = "hidden";
+      macos-option-as-alt = false;
+    };
+  };
+}
