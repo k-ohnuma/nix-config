a9887f134b273d3fcc8dd1916b231fe8f88092a6
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-16 08:27:38 +0900

    refactor: template統一

--- home/base/dev/procon.nix
+++ home/base/dev/procon.nix
@@ -6,19 +6,35 @@ let
     else
       "x86_64-unknown-linux-musl";
 
+  proconTemplateRs = builtins.readFile (libx.relativeToRoot "procon/template.rs");
+
   cargoAtcoderConfig = builtins.replaceStrings
-    [ "__CARGO_ATCODER_TARGET__" ]
-    [ cargoAtcoderTarget ]
+    [
+      "__CARGO_ATCODER_TARGET__"
+      "__PROCON_TEMPLATE_RS__"
+    ]
+    [
+      cargoAtcoderTarget
+      proconTemplateRs
+    ]
     (builtins.readFile (libx.relativeToRoot "procon/cargo-atcoder/cargo-atcoder.toml"));
+
+  cargoCompeteConfig = builtins.replaceStrings
+    [ "__PROCON_TEMPLATE_RS__" ]
+    [ proconTemplateRs ]
+    (builtins.readFile (libx.relativeToRoot "procon/cargo-compete/compete.toml"));
+
+  cargoVirtualConfig = builtins.replaceStrings
+    [ "__PROCON_TEMPLATE_RS__" ]
+    [ proconTemplateRs ]
+    (builtins.readFile (libx.relativeToRoot "procon/cargo-compete/virtual.toml"));
 in
 {
   xdg.configFile."cargo-atcoder.toml".text = cargoAtcoderConfig;
 
   home.file = {
-    "dev/atcoder/compete/compete.toml".source =
-      libx.relativeToRoot "procon/cargo-compete/compete.toml";
-    "dev/atcoder/virtual/compete.toml".source =
-      libx.relativeToRoot "procon/cargo-compete/virtual.toml";
+    "dev/atcoder/compete/compete.toml".text = cargoCompeteConfig;
+    "dev/atcoder/virtual/compete.toml".text = cargoVirtualConfig;
 
     "dev/atcoder/compete/.cargo/config.toml".source = libx.relativeToRoot "procon/config.toml";
     "dev/atcoder/virtual/.cargo/config.toml".source = libx.relativeToRoot "procon/config.toml";
---
+++ procon/template.rs
@@ -0,0 +1,9 @@
+#[rustfmt::skip]
+pub mod lib { pub use ac_library::*; pub use itertools::Itertools; pub use proconio::{input, marker::*}; pub use superslice::Ext; pub use std::cmp::*;}
+pub use lib::*;
+
+fn main() {
+    input! {
+
+    }
+}
