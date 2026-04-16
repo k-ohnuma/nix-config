c45f3bf89d610aa794171c27969f6929a016701b
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-17 08:06:56 +0900

    feat: finder config

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -14,11 +14,31 @@
     defaults = {
       menuExtraClock.Show24Hour = true;
       dock = {
+        # Dock のアイコンサイズ
         tilesize = 50;
+        # Dock を自動的に表示・非表示
         autohide = true;
+        # アプリ起動時のアニメーション
         launchanim = true;
+        # 起動中アプリのインジケーター（ドット）を表示
         show-process-indicators = true;
+        # 最近使ったアプリを Dock に表示しない
         show-recents = false;
+      };
+
+      finder = {
+        # タイトルバーにフルパス表示
+        _FXShowPosixPathInTitle = true;
+        # 拡張子を表示
+        AppleShowAllExtensions = true;
+        # 拡張子変更の警告
+        FXEnableExtensionChangeWarning = false;
+        # finderの終了ができるように
+        QuitMenuItem = true;
+        # パスバーの有効か
+        ShowPathbar = true;
+        # ステータスバーの有効化
+        ShowStatusBar = true;
       };
     };
   };
