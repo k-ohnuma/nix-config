62da64de8cd279c73f199dc6999b0d4ce8b8257e
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-17 08:06:33 +0900

    feat: macOSのシステムを即時反映できるように

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{
+  lib,
+  userName,
+  ...
+}:
 {
   time.timeZone = "Asia/Tokyo";
   system = {
+    activationScripts.postActivation.text = lib.mkAfter ''
+      # macOS の設定変更をログアウトなしで即時反映する
+      /usr/bin/sudo -u ${userName} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
     defaults = {
       menuExtraClock.Show24Hour = true;
       dock = {
