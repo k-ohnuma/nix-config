f7d406625bd414b7df9951270a8db9080fbe84b8
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-20 09:18:13 +0900

    update(fish): nccでディレクトリが存在したら削除してcargo compete

--- fish/ncc.fish
+++ fish/ncc.fish
@@ -5,6 +5,30 @@ function ncc --description "cargo compete new <contest> && cd <contest> && nvim 
     end
 
     set -l contest $argv[1]
+
+    if test -e "$contest"
+        if not test -d "$contest"
+            echo "error: '$contest' exists and is not a directory."
+            return 1
+        end
+
+        while true
+            read -l -P "Directory '$contest' already exists. Delete and recreate? [y/N]: " answer
+            set -l answer_normalized (string lower -- (string trim -- "$answer"))
+
+            switch "$answer_normalized"
+                case y yes
+                    rm -rf -- "$contest"
+                    break
+                case '' n no
+                    echo "canceled."
+                    return 1
+                case '*'
+                    echo "please answer y or n."
+            end
+        end
+    end
+
     cargo compete new $contest; or return $status
     cd $contest; and nvim .
 end
