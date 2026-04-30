5ad8c3a4adb64abf7b1d2bed38181e074ef73891
ohnuma <k.a.y.0818.k@gmail.com>
2026-04-30 14:24:09 +0900

    fix(host): git includeif

--- home/hosts/darwin/ohnuma.nix
+++ home/hosts/darwin/ohnuma.nix
@@ -8,9 +8,18 @@
   imports = [ ../../darwin ];
   programs.git = {
     signing.key = "${config.home.homeDirectory}/.ssh/id_ed25519_signers.pub";
-    settings = {
-      "includeIf \"gitdir:${vars.users.${userName}.git.gitDirPath}\"".path =
-        "${vars.users.${userName}.git.gitDirConfig}";
-    };
+    includes = [
+      {
+        condition = "gitdir:${vars.users.${userName}.git.gitDirPath}";
+        path = vars.users.${userName}.git.gitDirConfig;
+      }
+    ];
+    ignores = [
+      "flake.nix"
+      "flake.lock"
+      ".envrc"
+      "Justfile"
+      "justfile"
+    ];
   };
 }
