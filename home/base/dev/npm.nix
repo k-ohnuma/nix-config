3699b39d217dc7a4f3ce4c1ce08a4f967af23891
ohnuma <k.a.y.0818.k@gmail.com>
2026-06-12 14:14:43 +0900

    fix(npm): delete .npmrc from ohnuma profile

--- home/base/dev/npm.nix
+++ home/base/dev/npm.nix
@@ -1,5 +1,4 @@
 { libx, ... }:
 {
   xdg.configFile."pnpm/rc".source = libx.relativeToRoot "npm/.pnpmrc";
-  home.file.".npmrc".source = libx.relativeToRoot "npm/.npmrc";
 }
--- home/hosts/darwin/user.nix
+++ home/hosts/darwin/user.nix
@@ -1,5 +1,6 @@
-{ ... }:
+{ libx, ... }:
 {
   imports = [ ../../darwin ];
   # WIP: ここでこのホスト固有設定が欲しくなったら書く。とりあえずはdarwin共通設定
+  home.file.".npmrc".source = libx.relativeToRoot "npm/.npmrc";
 }
