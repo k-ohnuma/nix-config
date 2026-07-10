fa2f5900bfd842164b6ac7418d32fb0d41c750c2
ohnuma <k.a.y.0818.k@gmail.com>
2026-07-10 20:02:28 +0900

    fix(wezterm): import path

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,5 @@
-local config_dir = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
-if config_dir then
-  package.path = config_dir .. "?.lua;" .. package.path
-end
+local wezterm = require("wezterm")
+
+package.path = wezterm.config_dir .. "/?.lua;" .. package.path
 
 return require("darwin")
