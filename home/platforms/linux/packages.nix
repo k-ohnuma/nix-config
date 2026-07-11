49eada82656c743d25895cdf5f4811a881d501a8
ohnuma <k.a.y.0818.k@gmail.com>
2026-07-12 01:02:58 +0900

    chore(nixos): add openssl env

--- home/platforms/linux/packages.nix
+++ home/platforms/linux/packages.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
+let
+  inherit (pkgs) openssl;
+in
 {
+  home.sessionVariables = {
+    PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+    OPENSSL_DIR = "${openssl.dev}";
+    OPENSSL_LIB_DIR = "${openssl.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
+  };
+
   home.packages = with pkgs; [
     # Required by Neovim plugins such as nvim-treesitter when parsers are built locally.
     gcc
